f5ebad61770b9ea9d325ce150ec3c9b09cecb0af
Solus Bellator <tj.orga@gmx.de>
2023-07-16 01:28:10 +0200

    close to finishing c, co-star appearences are not counted correctly (all 1)

--- src/main/java/JDBCExerciseJavaImplementation.java
+++ src/main/java/JDBCExerciseJavaImplementation.java
@@ -2,11 +2,18 @@ import de.hpi.dbs1.ChosenImplementation;
 import de.hpi.dbs1.ConnectionConfig;
 import de.hpi.dbs1.JDBCExercise;
 import de.hpi.dbs1.entities.Actor;
+import de.hpi.dbs1.entities.ActorCounter;
 import de.hpi.dbs1.entities.Movie;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.*;
 import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @ChosenImplementation(true)
@@ -50,13 +57,100 @@ public class JDBCExerciseJavaImplementation implements JDBCExercise {
 	}
 
 	@Override
-	public List<Actor> queryActors(
-		@NotNull Connection connection,
-		@NotNull String keywords
-	) throws SQLException {
-		logger.info(keywords);
+	public List<Actor> queryActors(@NotNull Connection connection, @NotNull String keywords) throws SQLException
+	{
+		//////// RETRIEVE ACTORS ////////
+		PreparedStatement actorQuery =
+				connection.prepareStatement("SELECT " +
+						"nconst, primaryname " +
+						"FROM tprincipals NATURAL JOIN nbasics " +
+						"WHERE (category = 'actor' OR category = 'actress') AND ARRAY_LENGTH(knownfortitles, 1) > 0 " +
+						"AND primaryname LIKE ? " +
+						"GROUP BY nconst, primaryname, knownfortitles " +
+						"ORDER BY ARRAY_LENGTH(knownfortitles, 1) DESC, primaryname ASC " +
+						"LIMIT 5;");
+		actorQuery.setString(1, "%"+keywords+"%");
+		ResultSet result = actorQuery.executeQuery();
+
 		List<Actor> actors = new ArrayList<>();
 
-		throw new UnsupportedOperationException("Not yet implemented");
+		while (result.next())
+		{
+			String nconst = result.getString("nconst");
+			String primaryname = result.getString("primaryname");
+
+			Actor actor = new Actor(nconst, primaryname);
+
+			actors.add(actor);
+		}
+
+		//////// RETRIEVE RECENT MOVIES FOR EACH ACTOR ////////
+
+		for(Actor actor : actors)
+		{
+			PreparedStatement movieQuery = connection.prepareStatement("SELECT title FROM tprincipals NATURAL JOIN tmovies WHERE nconst = ? AND (category = 'actor' OR category = 'actress') AND year > 0 ORDER BY year DESC, title ASC LIMIT 5;");
+
+			movieQuery.setString(1, actor.nConst);
+			result = movieQuery.executeQuery();
+
+			while (result.next())
+			{
+				actor.playedIn.add(result.getString("title"));
+			}
+
+			PreparedStatement coStarAllMoviesQuery = connection.prepareStatement("SELECT tconst FROM tprincipals WHERE nconst = ? AND (category = 'actor' OR category = 'actress');");
+			coStarAllMoviesQuery.setString(1, actor.nConst);
+			ResultSet allMovies = coStarAllMoviesQuery.executeQuery();
+
+			List<ActorCounter> playedWith = new ArrayList<ActorCounter>();
+
+			while (allMovies.next())
+			{
+				PreparedStatement getAllCoActors = connection.prepareStatement("SELECT primaryname FROM nbasics NATURAL JOIN tprincipals WHERE tconst = ?;");
+				getAllCoActors.setString(1, allMovies.getString("tconst"));
+				ResultSet allActorsFromMovies = getAllCoActors.executeQuery();
+
+				while(allActorsFromMovies.next())
+				{
+					String name = allActorsFromMovies.getString("primaryname");
+					boolean contained = false;
+					int index = 0;
+
+					for(ActorCounter ac : playedWith)
+					{
+						if (ac.name == name)
+						{
+							index = playedWith.indexOf(ac);
+							contained = true;
+							break;
+						}
+					}
+
+					if(contained)
+					{
+						playedWith.get(index).increaseCount();
+					}
+					else
+					{
+						playedWith.add(new ActorCounter(name));
+					}
+
+				}
+			}
+
+			playedWith.sort(ActorCounter::compareTo);
+
+			for(ActorCounter ac : playedWith)
+			{
+				actor.costarNameToCount.put(ac.name, ac.count);
+			}
+
+
+		}
+
+
+
+
+		return actors;
 	}
 }
---
+++ src/main/java/de/hpi/dbs1/entities/ActorCounter.java
@@ -0,0 +1,40 @@
+package de.hpi.dbs1.entities;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ActorCounter implements Comparable
+{
+    public String name;
+    public int count;
+
+    public ActorCounter(String name)
+    {
+        this.name = name;
+        this.count = 1;
+    }
+
+    // returns n < 0 if ac has greater count OR equal count but lower name, n > 0 if reversed
+    //therefore sorts by count DESC, and within one count, name ASC
+    public int compareTo(@NotNull ActorCounter ac)
+    {
+        if(this.count != ac.count)
+        {
+            return this.count - ac.count;
+        }
+        else
+        {
+            return this.name.compareTo(ac.name);
+        }
+    }
+
+    public void increaseCount()
+    {
+        this.count++;
+    }
+
+    @Override
+    public int compareTo(@NotNull Object o)
+    {
+        return 0;
+    }
+}
